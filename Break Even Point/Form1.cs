0f000e86fd3c01334fc9d326ad347eaa9098b96e
GUTSMAN.21220 <GUTSMAN.21220@LABI1-13PC19>
2025-12-10 13:34:23 +0100

    Aggiungere i file di progetto.

---
+++ Break Even Point/Form1.cs
@@ -0,0 +1,40 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Break_Even_Point
+{
+    public partial class Form1 : Form
+    {
+        ScottPlot.Plot myPlot = new ScottPlot.Plot();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+            double[] xs = { 1, 2, 3, 4, 5 };
+            double[] ys = { 1, 4, 9, 16, 25 };
+
+         
+            myPlot.Add.Scatter(xs, ys);
+
+     
+            myPlot.SavePng("quickstart.png", 400, 300);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
